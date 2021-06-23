e72b8f19bbbbde03b18abeae81b2a0704184bd04
Debdeep-Maji <81363503+Debdeep-Maji@users.noreply.github.com>
2021-06-23 16:27:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -82,3 +82,43 @@ function my_key_down(e) {
     }
 
 }
+
+function up() {
+    if (player_y >= 0) {
+        player_y=player_y-block_image_height;
+        console.log("block_image_height= "+block_image_height);
+        console.log("When up arrow key is pressed,x= " +player_x+"y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+            }
+}
+
+function down() {
+    if (player_y <= 500) {
+        player_y=player_y+block_image_height;
+        console.log("block_image_height= "+block_image_height);
+        console.log("When up arrow key is pressed,x= " +player_x+"y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+            }
+}
+
+function left() {
+    if (player_x >= 0) {
+        player_x=player_x-block_image_height;
+        console.log("block_image_height= "+block_image_height);
+        console.log("When up arrow key is pressed,x= " +player_x+"y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+            }
+}
+
+function right() {
+    if (player_x <= 850) {
+        player_x=player_x+block_image_height;
+        console.log("block_image_height= "+block_image_height);
+        console.log("When up arrow key is pressed,x= " +player_x+"y= "+player_y);
+        canvas.remove(player_object);
+        player_update();
+            }
+}
